e44e2505dd7436ff1fd332e51736bb5e801b570a
EdvinElevNTI <115615340+EdvinElevNTI@users.noreply.github.com>
2024-05-15 09:05:15 +0000

    Data base initialized

--- factory.py
+++ factory.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 from blueprints import chart_bp, about_bp, elpris_bp, matsvinn_bp, setup_chart_socketio, setup_elpris_socketio
 from flask_socketio import SocketIO
+from flask_sqlalchemy import SQLAlchemy
 
 
 def create_app():
@@ -13,6 +14,12 @@ def create_app():
     
     setup_chart_socketio(socketio)
     setup_elpris_socketio(socketio)
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///song_library.db'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['SECRET_KEY'] = 'you-will-never-guess'
+
+    db = SQLAlchemy(app)
 
     @app.route("/", methods=["GET"])
     def home():
